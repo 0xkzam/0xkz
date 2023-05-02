f2df4ec57bf0d80721d65ca85b966227d78f4892
Kz <0xkzhunter@gmail.com>
2023-05-03 01:49:40 +0530

    New templated related mods

--- src/templates/index.ts
+++
@@ -1,7 +0,0 @@
-// import { default as BlogEntry } from '../templates_/BlogEntry'
-import { default as BlogList } from './BlogList'
-// import { default as BookReview } from './BookReview'
-// import { default as Tags } from './TagList'
-
-// export { BlogEntry, BlogList, BookReview, Tags, Stats }
-export {BlogList}
--- src/utils/index.js
+++
@@ -1,5 +0,0 @@
-const dateFns = require('./dateFns')
-const sortPosts = require('./sortPosts')
-const redirectNoteManually = require('./redirectNoteManually')
-
-module.exports = { ...dateFns, ...sortPosts, ...redirectNoteManually }
